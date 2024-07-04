fa296bf2d617fab0e1ea99539d89cd90a2856266
Teplov Andrei <Andrewspllen@gmail.com>
2024-07-04 14:06:51 +0300

    done it

---
+++ stats.py
@@ -0,0 +1,15 @@
+class Stats:
+    """Отслеживание статистики"""
+
+    def __init__(self):
+        """Инициализирует статистику"""
+        self.reset_stats()
+        self.run_game = True
+        with open('highscore.txt', 'r') as f:
+            self.high_score = int(f.readline())
+
+    def reset_stats(self):
+        """Статистика, изменяющаяся во время игры"""
+        self.guns_left = 2
+        self.score = 0
+
